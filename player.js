52bb33c3d573f6dee566c8132d58043154d18560
Utsav Chadha <utsavchadha@172-16-254-51.DYNAPOOL.NYU.EDU>
2017-02-23 13:52:04 -0500

    Added some mediocre visualization.

--- player.js
+++ player.js
@@ -1,39 +1,49 @@
 var synth = new Tone.Synth();
 
+/*var buffer = new Tone.Buffer("./audio/FWDL.mp3", function(){
+	//the buffer is now available.
+	var buff = buffer.get();
+});*/
+
+/*var sampler = new Tone.Sampler("https://tonejs.github.io/examples/audio/casio/A1.mp3", function() {
+  //repitch the sample down a half step
+	sampler.triggerAttack(-1);
+}).toMaster();*/
+
 var player = new Tone.Player({
   "url": "https://raw.githubusercontent.com/utsavchadha/samples/master/Documents/Samples/Soundpiece%20-%20Ragamala/Sitar%20Samples/089%20World%20Sitar_Bbm.wav",
   "loop": false,
   "loopstart": 0,
-  "loopend": 5
+  "loopend": 5,
 }).toMaster();
 
 var player2 = new Tone.Player({
   "url": "https://raw.githubusercontent.com/utsavchadha/samples/master/Documents/Samples/Soundpiece%20-%20Ragamala/Sitar%20Samples/sitar_two.wav",
-  "loop": false
+  "loop": false,
 }).toMaster();
 
 var player3 = new Tone.Player({
   "url": "https://raw.githubusercontent.com/utsavchadha/samples/master/Documents/Samples/Soundpiece%20-%20Ragamala/Sitar%20Samples/sitar_three.wav",
-  "loop": false
+  "loop": false,
 }).toMaster();
 
 var player4 = new Tone.Player({
   "url": "https://raw.githubusercontent.com/utsavchadha/samples/master/Documents/Samples/Soundpiece%20-%20Ragamala/Pro%20Tools/Rock%20Hard/Drums/Rock%20Hard/077%20Fat%20Rock%20Drums%20C-Hat.wav",
-  "loop": false
+  "loop": false,
 }).toMaster();
 
 var player5 = new Tone.Player({
   "url": "https://raw.githubusercontent.com/utsavchadha/samples/master/Documents/Samples/Soundpiece%20-%20Ragamala/Pro%20Tools/Rock%20Hard/Bass/Rock/bass_one.wav",
-  "loop": true
+  "loop": true,
 }).toMaster();
 
 Tone.Buffer.on("load", function() {
   //player.start();
-  player5.start(2);
-  player5.stop(25);
-  player2.start(0);
-  player3.start(15);
-  player4.start(10);
+  //player5.start(2);
+  //player5.stop(25);
+  //player2.start(0);
+  //player3.start(15);
+  //player4.start(10);
 }.bind(this));
 
 synth.toMaster();
@@ -44,6 +54,6 @@ var pattern = new Tone.Pattern(function(time, note) {
   synth.triggerAttackRelease(note, 0.25);
 }, ["C4", "E4", "G4", "A4"]);
 
-pattern.start(0);
-Tone.Transport.start();
-Tone.Transport.stop(15);
+//pattern.start(0);
+//Tone.Transport.start();
+//Tone.Transport.stop(15);
--- sketch.js
+++ sketch.js
@@ -1,3 +1,16 @@
+var bassimg, drumimg, sitarimg, synthimg;
+var bassPlay = 0;
+var drumPlay = 0;
+var synthPlay = 0;
+var sitarPlay = 0;
+
+function preload() {
+  bassimg = loadImage("assets/Bassist.jpg");
+  drumimg = loadImage("assets/Drums.jpg");
+  sitarimg = loadImage("assets/Sitarist.jpg");
+  synthimg = loadImage("assets/Synthist.jpg");
+}
+
 function setup() {
   createCanvas(800, 600);
   background(0);
@@ -9,15 +22,62 @@ function draw() {
 }
 
 function createSections() {
-  fill(10);
-  rect(0, 0, 400, 300);
+  Tone.Buffer.on("load", function() {
 
-  fill(70);
-  rect(400, 0, 400, 300);
+    fill(10);
+    image(bassimg, 0, 0);
 
-  fill(140);
-  rect(0, 300, 400, 300);
+    fill(70);
+    image(drumimg, 400, 0);
 
-  fill(210);
-  rect(400, 300, 400, 300);
+    fill(140);
+    image(sitarimg, 0, 300);
+
+    fill(210);
+    image(synthimg, 400, 300);
+  }.bind(this));
+}
+
+function mouseClicked() {
+  if ((mouseX > 0 && mouseX < 400) && (mouseY > 0 && mouseY < 300)) {
+    if (bassPlay == 0) {
+      player5.start();
+      bassPlay = 1;
+    } else if (bassPlay == 1) {
+      //player5.stop();
+      bassPlay = 0;
+    }
+  }
+
+  if ((mouseX > 400 && mouseX < 800) && (mouseY > 0 && mouseY < 300)) {
+    if (drumPlay == 0) {
+      player4.start();
+      drumPlay = 1;
+    } else {
+      player4.stop();
+      drumPlay = 0;
+    }
+  }
+
+  if ((mouseX > 0 && mouseX < 400) && (mouseY > 300 && mouseY < 600)) {
+    if (sitarPlay == 0) {
+      player3.start();
+      sitarPlay = 1;
+    } else {
+      player3.stop();
+      sitarPlay = 0;
+    }
+  }
+
+  if ((mouseX > 400 && mouseX < 800) && (mouseY > 300 && mouseY < 600)) {
+    if (synthPlay == 0) {
+      pattern.start();
+      Tone.Transport.start();
+      synthPlay = 1;
+    } else {
+      pattern.stop();
+      Tone.Transport.stop();
+      synthPlay = 0;
+    }
+  }
 }
