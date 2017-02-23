69ffa54f40ab8d8308b5294de20da8e34e40a8d0
Utsav Chadha <utsavchadha@Utsavs-MBP.home>
2017-02-23 10:23:21 -0500

    Correcting canvas declaration.

--- sketch.js
+++ sketch.js
@@ -1,23 +1,23 @@
 function setup() {
-  canvas(800,600);
+  createCanvas(800, 600);
   background(0);
+  createSections();
 }
 
 function draw() {
-  createSections();
-  background(0);
+
 }
 
 function createSections() {
   fill(10);
-  rect(0,0,400,300);
-  
+  rect(0, 0, 400, 300);
+
   fill(70);
-  rect(400,0,400,300);
-  
+  rect(400, 0, 400, 300);
+
   fill(140);
-  rect(0,300,400,300);
-  
+  rect(0, 300, 400, 300);
+
   fill(210);
-  rect(400,300,400,300);
+  rect(400, 300, 400, 300);
 }
